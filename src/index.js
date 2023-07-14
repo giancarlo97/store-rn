aadd4bcdfc5a1b3904b71996e51fd948716793a8
Giancarlo Oblitas <giancarlooblitas@gmail.com>
2023-07-13 23:11:48 -0300

    products

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
+import { useState } from 'react';
 import { SafeAreaView, View, StyleSheet, FlatList } from 'react-native';
 
-import { CategoryItem, Header } from './components';
-import CATEGORIES from './constants/data/categories.json';
+import { Header } from './components';
+import { Categories, Products } from './screens';
+import { COLORS } from './themes';
+
+const categoryDefault = {
+  categoryId: null,
+  color: COLORS.primary,
+};
 
 export default function App() {
-  const onSelectCategory = (categoryId) => {
-    console.warn({ categoryId });
+  const [isCategorySelected, setIsCategorySelected] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(categoryDefault);
+
+  const headerTitle = isCategorySelected ? 'Products' : 'Categories';
+
+  const onHandleSelectCategory = ({categoryId, color }) => {
+    setSelectedCategory({ categoryId, color });
+    setIsCategorySelected(!isCategorySelected);
   };
+  const onHandleNavigate = () => {
+    setIsCategorySelected(!isCategorySelected);
+    setSelectedCategory(categoryDefault);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
-        <Header title="Categories" />
-        <FlatList
-          data={CATEGORIES}
-          style={styles.categoryContainer}
-          contentContainerStyle={styles.listCategory}
-          renderItem={({ item }) => <CategoryItem {...item} onSelectCategory={onSelectCategory} />}
-          keyExtractor={(item) => item.id}
-          showsVerticalScrollIndicator={false}
-        />
+      <Header title={headerTitle} style={{ backgroundColor: selectedCategory.color }} />
+        {isCategorySelected ? (
+          <Products onHandleGoBack={onHandleNavigate} categorySelected={selectedCategory} />
+        ) : (
+          <Categories onSelectCategory={onHandleSelectCategory} />
+        )}
       </View>
     </SafeAreaView>
   );
@@ -27,14 +42,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  categoryContainer: {
-    marginTop: 15,
-    marginHorizontal: 15,
-  },
-  listCategory: {
-    gap: 15,
-    paddingBottom: 20,
   },
 });
