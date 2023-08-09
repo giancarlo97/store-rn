977c836756d5a3ca538305f8451256d48be561bf
Giancarlo Oblitas <giancarlooblitas@gmail.com>
2023-08-09 03:32:28 -0300

    create api key

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,2 +1,14 @@
 export const URL_BASE_FIREBASE_REALTIME_DATABASE = 
 'https://shopdos-d6e17-default-rtdb.firebaseio.com';
+
+export const FIREBASE_API_KEY = 
+'AIzaSyD5keTEFKwlYIn5QJG-QsfAer77bNht9z8';
+
+export const FIREBASE_AUTH_BASE_URL = 
+'https://identitytoolkit.googleapis.com/v1/accounts';
+
+export const FIREBASE_AUTH_SIGN_IN_API_URL = 
+`:signInWithPassword?key=${FIREBASE_API_KEY}`;
+
+export const FIREBAS_AUTH_SIGN_UP_API_URL = 
+`:signUp?key=${FIREBASE_API_KEY}`;
--- src/store/index.js
+++ src/store/index.js
@@ -6,21 +6,26 @@ import categoriesReducer from './categories/categories.slice';
 import { categoriesApi } from './categories/api';
 import { productsApi } from './products/api';
 import { ordersApi } from './orders/api';
+import { authApi } from './auth/api';
+import authReducer from './auth/auth.slice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     categories: categoriesReducer,
     cart: cartReducer,
+    auth: authReducer,
     [categoriesApi.reducerPath]: categoriesApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
     [ordersApi.reducerPath]: ordersApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware().concat(
       categoriesApi.middleware,
       productsApi.middleware,
-      ordersApi.middleware
+      ordersApi.middleware,
+      authApi.middleware
     ),
 });
 
