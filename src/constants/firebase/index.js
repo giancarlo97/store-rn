bcc843ab4d35186c735823bad483bd20f93f841f
Giancarlo Oblitas <giancarlooblitas@gmail.com>
2023-08-31 01:56:09 -0300

    maps updates

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,8 +1,10 @@
 export const URL_BASE_FIREBASE_REALTIME_DATABASE = 
-'https://shopdos-d6e17-default-rtdb.firebaseio.com';
+//'https://shopdos-d6e17-default-rtdb.firebaseio.com';
+'https://store-rn-74889-default-rtdb.firebaseio.com';
 
 export const FIREBASE_API_KEY = 
-'AIzaSyD5keTEFKwlYIn5QJG-QsfAer77bNht9z8';
+//'AIzaSyD5keTEFKwlYIn5QJG-QsfAer77bNht9z8';
+'AIzaSyBqm5MPhw8oGrcpdQ38-y5Fqw_R9ncleMU';
 
 export const FIREBASE_AUTH_BASE_URL = 
 'https://identitytoolkit.googleapis.com/v1/accounts';
@@ -16,4 +18,4 @@ export const FIREBAS_AUTH_SIGN_UP_API_URL =
 //export const GOOGLE_API_KEY = 
 //'AIzaSyC2izRsT4Fp-sc-hJ74RfJG7c-i-wu_Abk';
 
-//AIzaSyC3_3Hh1d5CJ-Z69G9beHLmfhzQURJHFHc  --este es mi api KEY DE GOOGLE
+//AIzaSyBToUmkRY-5zpCYRKm6fQwYyMXLz3lQBlE  --este es mi api KEY DE GOOGLE
